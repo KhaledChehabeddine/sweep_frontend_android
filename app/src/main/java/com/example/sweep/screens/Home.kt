f7d933689fd70a93a910f4026f5ebdb5c5cdd77a
Khaled Chehabeddine <khaled.ch2002@gmail.com>
2023-02-13 22:28:53 +0200

    Changed clickable box shape and shimmer color

--- app/src/main/java/com/example/sweep/screens/Home.kt
+++ app/src/main/java/com/example/sweep/screens/Home.kt
@@ -2,20 +2,24 @@ package com.example.sweep.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.sweep.ui.theme.SweepTheme
 import com.example.sweep.utilities.ServiceCategoryGrid
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(paddingValues: PaddingValues) {
     Surface(
@@ -31,11 +35,20 @@ fun HomeScreen(paddingValues: PaddingValues) {
                                        .padding(all = 20.dp)
                 ) {
                     Row {
-                        Card(
-                            colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.secondaryContainer),
+                        Box(
                             modifier = Modifier.height(height = 225.dp)
                                                .fillMaxWidth()
-                                               .clickable {
+                                               .clip(RoundedCornerShape(percent = 8))
+                                               .background(color = MaterialTheme.colorScheme.secondaryContainer)
+                                               .clickable(
+                                                   indication = rememberRipple(
+                                                       color = MaterialTheme.colorScheme
+                                                           .onSecondaryContainer
+                                                   ),
+                                                   interactionSource = remember {
+                                                       MutableInteractionSource()
+                                                   }
+                                               ) {
                                                     /* TODO */
                                                }
                         ) {
@@ -87,18 +100,26 @@ fun HomeScreen(paddingValues: PaddingValues) {
                         )) {
                             item {
                                 for (j in 1..3) {
-                                    Card(
-                                        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.secondaryContainer),
-                                        modifier = Modifier.size(
-                                            height = 160.dp,
-                                            width = 250.dp
-                                        ).padding(end = 20.dp)
-                                            .clickable {
-                                                /* TODO */
-                                            }
-                                    ) {
-
-                                    }
+                                    Box(modifier = Modifier.size(
+                                                                height = 160.dp,
+                                                                width = 250.dp
+                                                          ).padding(end = 20.dp)
+                                                           .clip(RoundedCornerShape(percent = 8))
+                                                           .background(
+                                                               color = MaterialTheme.colorScheme.secondaryContainer
+                                                           )
+                                                           .clickable(
+                                                               indication = rememberRipple(
+                                                                   color = MaterialTheme.colorScheme
+                                                                                        .onSecondaryContainer
+                                                               ),
+                                                               interactionSource = remember {
+                                                                     MutableInteractionSource()
+                                                               }
+                                                           ) {
+                                                                /* TODO */
+                                                           }
+                                    )
                                 }
                             }
                         }
